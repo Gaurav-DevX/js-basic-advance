9119939ea08b813edadfd7c7e3513a3801bd2c52
Mishra Gaurav <67998380+Gaurav-DevX@users.noreply.github.com>
2024-09-05 04:29:39 +0000

    05 string updated

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,35 +1,52 @@
-const name = "gaurav"
-const repoCount = 50
+// const name = "gaurav"
+// const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+// // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // String interpolation.
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // String interpolation.
 
-const gameName = new String('gaurav-hc-com')
+// const gameName = new String('gaurav-hc-com')
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// // console.log(gameName[0]);
+// // console.log(gameName.__proto__);
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
-console.log(newString);
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
 
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
 
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newStringOne = "   gaurav    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+// const url = "https://hitesh.com/hitesh%20choudhary"
 
-console.log(url.replace('%20', '-'))
+// console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+// console.log(url.includes('sundar'))
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
+
+// console.log(gameName.at(3));
+
+const anotherGame = 'hulalaahulaaa';
+// console.log(anotherGame.charAt(5)); // It gives character at the given index;
+// console.log(anotherGame.charCodeAt(4)); // The zero-based index of the desired character at specified index, "NaN" is returned. Returns the Unicode value of the specified location.
+
+// console.log(anotherGame.codePointAt(13)); // Same as 'charCodeAt' but difference is that it's return "undefined" when specified index is not available;
+
+// console.log(anotherGame.concat('khuljaseemseem')); // The String to append to the end of the string . Returns a new string that contains the concatenation of two or more strings;
+
+console.log(anotherGame.);
+
+
+
+
+
