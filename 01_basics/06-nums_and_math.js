f4d4c2e2641d1fe4aab5256a9f074f564d9ddc9f
Mishra Gaurav <67998380+Gaurav-DevX@users.noreply.github.com>
2024-09-05 03:13:53 +0000

    About numbers and math functions

---
+++ 01_basics/06-nums_and_math.js
@@ -0,0 +1,25 @@
+// const score = 400
+
+// console.log(score);
+
+// const bankBalance = new Number(4000); // Assing number value explicitely;
+
+// console.log(bankBalance);
+
+// console.log(bankBalance.toString());
+// console.log(bankBalance.toString().length);
+// console.log(bankBalance.toFixed(2));
+
+// const anotherNumber = 356.989;
+// console.log(anotherNumber.toPrecision(3));
+
+// const hundreds = 10021115;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+console.log(Math);
+
+
+
